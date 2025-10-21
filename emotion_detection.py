c6c31ed367b7ad671585eca69fa85d496df32395
CyPietro17 <pietro.salvatore95@protonmail.com>
2025-10-21 05:17:18 -0400

    Converted output in json

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,34 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     obj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = obj, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = float(emotions['anger'])
+    disgust_score = float(emotions['disgust'])
+    fear_score = float(emotions['fear'])
+    joy_score = float(emotions['joy'])
+    sadness_score = float(emotions['sadness'])
+    
+    dominant_emotion = "dominant_emotion"
+    dominant_score = 0.0
+
+    for emotion in emotions:
+        emotion_score = float(emotions[emotion])
+        if dominant_score < emotion_score:
+            dominant_score = emotion_score
+            dominant_emotion = emotion
+    
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score, 
+        'fear': fear_score, 
+        'joy': joy_score, 
+        'sadness': sadness_score, 
+        'dominant_emotion': dominant_emotion
+        }
