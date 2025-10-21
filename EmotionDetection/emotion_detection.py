870acab7d3f334bbcaac03676fdd5af435fe5494
CyPietro17 <pietro.salvatore95@protonmail.com>
2025-10-21 07:01:24 -0400

    Incorporate Error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,21 +8,30 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json = obj, headers=header)
     formatted_response = json.loads(response.text)
 
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    anger_score = float(emotions['anger'])
-    disgust_score = float(emotions['disgust'])
-    fear_score = float(emotions['fear'])
-    joy_score = float(emotions['joy'])
-    sadness_score = float(emotions['sadness'])
+    if response.status_code == 200:
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        anger_score = float(emotions['anger'])
+        disgust_score = float(emotions['disgust'])
+        fear_score = float(emotions['fear'])
+        joy_score = float(emotions['joy'])
+        sadness_score = float(emotions['sadness'])
     
-    dominant_emotion = "dominant_emotion"
-    dominant_score = 0.0
+        dominant_emotion = "dominant_emotion"
+        dominant_score = 0.0
 
-    for emotion in emotions:
-        emotion_score = float(emotions[emotion])
-        if dominant_score < emotion_score:
-            dominant_score = emotion_score
-            dominant_emotion = emotion
+        for emotion in emotions:
+            emotion_score = float(emotions[emotion])
+            if dominant_score < emotion_score:
+                dominant_score = emotion_score
+                dominant_emotion = emotion
+
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
     
     return {
         'anger': anger_score,
