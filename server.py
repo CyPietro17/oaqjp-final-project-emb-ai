d2e0a01cef3b9f29c479b7a7a7c3bd9bee6bd99a
CyPietro17 <pietro.salvatore95@protonmail.com>
2025-10-21 07:18:50 -0400

    Pylint refactoring

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+""" Deploy web application using Flask. """
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +7,12 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """ Function rendering at the home page. """
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """ Function analyzing the input text and provide the correct output. """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
@@ -22,7 +26,12 @@ def sent_analyzer():
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
 
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+    response_1 = "For the given statement, the system response is "
+    response_2 = f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+    response_3 = f"'joy': {joy} and 'sadness': {sadness}. "
+    response_4 = f"The dominant emotion is {dominant_emotion}."
+
+    return response_1 + response_2 + response_3 + response_4
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
